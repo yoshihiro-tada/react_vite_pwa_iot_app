242255985d3b3ad2b39555033b9dbaf560e27666
yoshihiro-tada <shyuwatx@gmail.com>
2024-01-29 17:36:04 +0900

    スタイルブラッシュアップ

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -3,8 +3,17 @@ import { Link } from "react-router-dom";
 function Mypage() {
   return (
     <>
-      <div className="flex justify-between mb-2">
-        マイページ
+      <h2 className="w-fit">マイページ<span className="block">my page</span></h2>
+      <div className="my-4">
+        <div>氏名を変更する</div>
+        <div className="flex">
+          <input className="mr-2 border-gray-400 border-2 border-solid rounded-lg" type="text" placeholder="氏名"></input>
+          <button className="text-white bg-blue-600 px-2 py-1 rounded-lg shadow-md">確定する</button>
+        </div>
+      </div>
+      <div className="my-4">
+        <Link to={`/details`} className="text-white bg-blue-600 px-2 py-1 rounded-lg shadow-md block my-2 w-fit">担当機器一覧へ</Link>
+        <button className="text-white bg-blue-600 px-2 py-1 rounded-lg shadow-md block my-2">ログアウトする</button>
       </div>
       <Link to={`/`} className="block text-blue-600 w-fit text-sm">TOPへ</Link>
     </>
--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -7,10 +7,18 @@ function Tutorial() {
       <div className="mb-4">
         <h3 className="border-l-4 border-blue-600 pl-1 pt-1 pb-1 mb-1">氏名を登録しよう</h3>
         <p>※左詰め、フルネームでお願いいたします。</p>
+        <div className="my-4">
+          <div>氏名を登録する</div>
+          <div className="flex">
+            <input className="mr-2 border-gray-400 border-2 border-solid rounded-lg" type="text" placeholder="氏名"></input>
+            <button className="text-white bg-blue-600 px-2 py-1 rounded-lg shadow-md">確定する</button>
+          </div>
+        </div>
       </div>
       <div className="mb-4">
         <h3 className="border-l-4 border-blue-600 pl-1 pt-1 pb-1 mb-1">機器の詳細を確認しよう</h3>
         <p>機械場マップから機器の一覧が確認できます。</p>
+        <Link to={`/overview`} className="block text-blue-600 w-fit text-sm">機械場マップへ</Link>
       </div>
       <Link to={`/`} className="block text-blue-600 w-fit text-sm">TOPへ</Link>
     </>
